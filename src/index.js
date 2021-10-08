6162a9a46d899bb489a639fb53596ba833be94f5
Luiggy Macias <ferrinluiggy@gmail.com>
2021-10-08 21:04:04 +0000

    nodemon added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const { json } = require('stream/consumers');
 const app = express();
 
 app.get('/', (req, res) => {
-    json.send('aprendiendo mern stack')
+    res.json({})
 })
+
+// Settings
+
+
+// Middlewares
+
+
+// Static files
 app.listen(3000, () => {
     console.log('server is listen on port 3000');
 })
