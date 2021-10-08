44b7946b2b59ef7ad13a745c7ea1c5675d6ddbd8
Luiggy Macias <ferrinluiggy@gmail.com>
2021-10-08 21:08:03 +0000

    deeeee

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({})
+})
+
+// Settings
+
+
+// Middlewares
+
+
+// Static files
+app.listen(3000, () => {
+    console.log('server is listen on port 3000');
+})
