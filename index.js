d47e66dd4aa4d178128062374587cb17da6cb080
Luiggy Macias <ferrinluiggy@gmail.com>
2021-10-11 18:18:51 +0000

    vamos a ver

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-const app = require('./src/index.js');
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const path = require('path');
+require('dotenv').config();
 
+const { mongoose } = require('./src/database');
+
+// Settings 
+//app.set('port', process.env.PORT || 3000)
+
+// Middlewares
+app.use(morgan('dev'));
+//esto verifica que lo que se pide esté en formato json
+//esto reemplaza a bodyParser que hace la misma comprobacion
+app.use(express.json())
+
+// Routes
+//indica donde quires usar el modulo exportado (primer argumento), y el modulo en si (2do arg)
+app.use('/api/tasks', require('./src/routes/task.routes'));
+// Static files
+app.use(express.static(path.join(__dirname, 'src', 'public')));
+// Starting the server
+let port = process.env.PORT;
+app.listen(port, () => {
+    console.log('server is listening on port ' + port);
+})
