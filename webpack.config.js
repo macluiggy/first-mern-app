f8cf69bf0773109857f1a0cb4ae36339860dc877
Luiggy Macias <ferrinluiggy@gmail.com>
2021-10-11 20:47:42 +0000

    using babel and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,14 @@ module.exports = {
     output: {
         path: __dirname + '/src/public',
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.(js|jsx|tsx)$/,
+                exclude: /node_modules/
+            }
+        ]
     }
 }
