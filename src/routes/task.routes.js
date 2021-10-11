9ddda262c7da228e30b8566fd17c1b96859df94b
Luiggy Macias <ferrinluiggy@gmail.com>
2021-10-11 16:49:01 +0000

    using methods: GET, POST and PUT

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -4,13 +4,33 @@ const router = express.Router();
 const Task = require('../models/task');
 
 //este nos permite manejar las rutas de las peticiones, en este caso la ruta root
-router.get('/', (req, res) => {
-    Task.find(function (err, tasks) {
-        console.log(tasks);
-    })
+router.get('/', async (req, res) => {
+    const tasks = await Task.find()
+    //console.log(tasks, tasks.status);
     res.json({
-        status: 'API Works'
+        status: tasks
     });
+})
+
+router.post('/', async (req, res) => {
+    const { title, description } = req.body;
+    const task = new Task({ title, description });
+    await task.save()
+    res.json({
+        status: 'Tarea guardada'
+    })
+})
+
+router.put('/:id', async (req, res) => {
+    const { title, description } = req.body;
+    const newTask = { title, description };
+    await Task.findByIdAndUpdate(
+        req.params.id,
+        newTask
+    )
+    res.json({
+        status: newTask
+    })
 })
 
 module.exports = router;
