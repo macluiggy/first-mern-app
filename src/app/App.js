a1236fa0d5d4157dcbc80edecf8aeff32193ed28
Luiggy Macias <ferrinluiggy@gmail.com>
2021-10-11 21:45:42 +0000

    managin events

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,62 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            title: '',
+            Description: '',
+        };
+        this.addTask = this.addTask.bind(this);
+    }
+    addTask(e) {
+        console.log(this.state);
+        e.preventDefault();
+    }
+
+    handleChange(e) {
+        console.log(e.target.value);
+    }
+
     render() {
         return (
-            <h1>Hello Worldsss</h1>
+            <div>
+            {/* NAVIGATION */}
+                <nav className="light-blue darken-4">
+                    <div className="container">
+                        <a className="brand-logo" href="/">MERN Stack</a>
+                    </div>
+                </nav>
+                <div className="container">
+                    <div className="row">
+                        <div className="col s5">
+                            <div className="card">
+                                <div className="card-content">
+                                    <form onSubmit={this.addTask}>
+                                        <div className="row">
+                                            <div className="input-field col s12">
+                                                <input name="title" onChange={this.handleChange} type="text" placeholder="Task Title" />
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="input-field col s12">
+                                                <textarea name="description" onChange={this.handleChange} placeholder="Task Description" className="materialize-textarea"></textarea>
+                                            </div>
+                                        </div>
+                                        <button type="submit" className="btn light-blue darken-4">
+                                            Send
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col s7">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
